fbb1b13a34b0f0f2e3ad9b9e628472fa829f9ef4
Peter Adam <pedda@p3dda.net>
2013-03-06 07:12:31 +0100

    Remove selection polygon from OpenLayers Map on unselect event in plots

--- settings.py
+++ settings.py
@@ -49,6 +49,8 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+WUNDERGROUND_KEY = '8424e446a37c8faf'
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')

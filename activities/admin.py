548fae01a908a6159e9b6a6f26e2b6bf5dc3a149
Peter Adam <pedda@p3dda.net>
2013-09-14 10:12:02 +0200

    Replaced space-tabs with tabs

--- activities/admin.py
+++ activities/admin.py
@@ -5,96 +5,96 @@ from activities.models import Sport
 from django.contrib import admin
 
 class ActivityAdmin(admin.ModelAdmin):
-    list_display=('name', 'user')
-    
-    def save_model(self, request, obj, form, change):
-        print "Saving object"
-        if not change:
-            print "Not change, set user %s" % request.user
-            obj.user = request.user
-        obj.save()
-    
-    def queryset(self, request):
-        if request.user.is_superuser:
-            return Activity.objects.all()
-        return Activity.objects.filter(user=request.user)
-    
-    def has_change_permission(self, request, obj=None):
-        has_class_permission = super(ActivityAdmin, self).has_change_permission(request, obj)
-        if not has_class_permission:
-            return False
-        if obj is not None and not request.user.is_superuser and request.user != obj.user:
-            return False
-        return True
+	list_display=('name', 'user')
+	
+	def save_model(self, request, obj, form, change):
+		print "Saving object"
+		if not change:
+			print "Not change, set user %s" % request.user
+			obj.user = request.user
+		obj.save()
+	
+	def queryset(self, request):
+		if request.user.is_superuser:
+			return Activity.objects.all()
+		return Activity.objects.filter(user=request.user)
+	
+	def has_change_permission(self, request, obj=None):
+		has_class_permission = super(ActivityAdmin, self).has_change_permission(request, obj)
+		if not has_class_permission:
+			return False
+		if obj is not None and not request.user.is_superuser and request.user != obj.user:
+			return False
+		return True
 
 class EquipmentAdmin(admin.ModelAdmin):
-    list_display=('name', 'user')
-    
-    def save_model(self, request, obj, form, change):
-        print "Saving object"
-        if not change:
-            print "Not change, set user %s" % request.user
-            obj.user = request.user
-        obj.save()
-    
-    def queryset(self, request):
-        if request.user.is_superuser:
-            return Equipment.objects.all()
-        return Equipment.objects.filter(user=request.user)
-    
-    def has_change_permission(self, request, obj=None):
-        has_class_permission = super(EquipmentAdmin, self).has_change_permission(request, obj)
-        if not has_class_permission:
-            return False
-        if obj is not None and not request.user.is_superuser and request.user != obj.user:
-            return False
-        return True
+	list_display=('name', 'user')
+	
+	def save_model(self, request, obj, form, change):
+		print "Saving object"
+		if not change:
+			print "Not change, set user %s" % request.user
+			obj.user = request.user
+		obj.save()
+	
+	def queryset(self, request):
+		if request.user.is_superuser:
+			return Equipment.objects.all()
+		return Equipment.objects.filter(user=request.user)
+	
+	def has_change_permission(self, request, obj=None):
+		has_class_permission = super(EquipmentAdmin, self).has_change_permission(request, obj)
+		if not has_class_permission:
+			return False
+		if obj is not None and not request.user.is_superuser and request.user != obj.user:
+			return False
+		return True
 
 class SportAdmin(admin.ModelAdmin):
-    list_display=('name', 'color', 'user', 'speed_as_pace')
-    
-    def save_model(self, request, obj, form, change):
-        print "Saving object"
-        if not change:
-            print "Not change, set user %s" % request.user
-            obj.user = request.user
-        obj.save()
-    
-    def queryset(self, request):
-        if request.user.is_superuser:
-            return Sport.objects.all()
-        return Sport.objects.filter(user=request.user)
-    
-    def has_change_permission(self, request, obj=None):
-        has_class_permission = super(SportAdmin, self).has_change_permission(request, obj)
-        if not has_class_permission:
-            return False
-        if obj is not None and not request.user.is_superuser and request.user != obj.user:
-            return False
-        return True
+	list_display=('name', 'color', 'user', 'speed_as_pace')
+	
+	def save_model(self, request, obj, form, change):
+		print "Saving object"
+		if not change:
+			print "Not change, set user %s" % request.user
+			obj.user = request.user
+		obj.save()
+	
+	def queryset(self, request):
+		if request.user.is_superuser:
+			return Sport.objects.all()
+		return Sport.objects.filter(user=request.user)
+	
+	def has_change_permission(self, request, obj=None):
+		has_class_permission = super(SportAdmin, self).has_change_permission(request, obj)
+		if not has_class_permission:
+			return False
+		if obj is not None and not request.user.is_superuser and request.user != obj.user:
+			return False
+		return True
 
 class EventAdmin(admin.ModelAdmin):
-    list_display=('name', 'user')
+	list_display=('name', 'user')
 
-    def save_model(self, request, obj, form, change):
-        print "Saving object"
-        if not change:
-            print "Not change, set user %s" % request.user
-            obj.user = request.user
-        obj.save()
+	def save_model(self, request, obj, form, change):
+		print "Saving object"
+		if not change:
+			print "Not change, set user %s" % request.user
+			obj.user = request.user
+		obj.save()
 
-    def queryset(self, request):
-        if request.user.is_superuser:
-            return Event.objects.all()
-        return Event.objects.filter(user=request.user)
+	def queryset(self, request):
+		if request.user.is_superuser:
+			return Event.objects.all()
+		return Event.objects.filter(user=request.user)
 
-    def has_change_permission(self, request, obj=None):
-        has_class_permission = super(EventAdmin, self).has_change_permission(request, obj)
-        if not has_class_permission:
-            return False
-        if obj is not None and not request.user.is_superuser and request.user != obj.user:
-                    return False
-        return True
+	def has_change_permission(self, request, obj=None):
+		has_class_permission = super(EventAdmin, self).has_change_permission(request, obj)
+		if not has_class_permission:
+			return False
+		if obj is not None and not request.user.is_superuser and request.user != obj.user:
+					return False
+		return True
 
 admin.site.register(Activity, ActivityAdmin)
 admin.site.register(Equipment, EquipmentAdmin)
--- activities/preview.py
+++ activities/preview.py
@@ -10,31 +10,30 @@ from django.core.files.base import File
 from activities.utils import TCXTrack
 
 def create_preview(track):
-    logging.debug("Creating preview image for track")
-    if track:
-        tcxtrack = TCXTrack(track)
+	logging.debug("Creating preview image for track")
+	if track:
+		tcxtrack = TCXTrack(track)
 
-        pos_list = tcxtrack.get_pos(90)
-        if len(pos_list) > 1:
-            gmap_coords = []
-            for (lat, lon) in pos_list:
-                gmap_coords.append("%s,%s" % (round(lat, 4), round(lon, 4)))
-            gmap_path = "|".join(gmap_coords)
+		pos_list = tcxtrack.get_pos(90)
+		if len(pos_list) > 1:
+			gmap_coords = []
+			for (lat, lon) in pos_list:
+				gmap_coords.append("%s,%s" % (round(lat, 4), round(lon, 4)))
+			gmap_path = "|".join(gmap_coords)
 
-            url = "http://maps.google.com/maps/api/staticmap?size=480x480&path=color:0xff0000ff|"+gmap_path+"&sensor=true"
-            logging.debug("Fetching file from %s" % url)
-            logging.debug("Length of url is %s chars" % len(url))
-            try:
-                img_temp = NamedTemporaryFile(delete=True)
-                img_temp.write(urllib2.urlopen(url).read())
-                img_temp.flush()
-                name=os.path.splitext(os.path.split(track.trackfile.name)[1])[0]
-                logging.debug("Saving as %s.jpg" % name)
-
-                track.preview_img.save("%s.jpg" % name, File(img_temp), save=True)
-            except urllib2.URLError, exc:
-                logging.error("Exception occured when creating preview image: %s" % exc)
-        else:
-            logging.debug("Track has no GPS position data, not creating preview image")
-            # TODO: Maybe load fallback image as preview_image
-
+			url = "http://maps.google.com/maps/api/staticmap?size=480x480&path=color:0xff0000ff|"+gmap_path+"&sensor=true"
+			logging.debug("Fetching file from %s" % url)
+			logging.debug("Length of url is %s chars" % len(url))
+			try:
+				img_temp = NamedTemporaryFile(delete=True)
+				img_temp.write(urllib2.urlopen(url).read())
+				img_temp.flush()
+				name=os.path.splitext(os.path.split(track.trackfile.name)[1])[0]
+				logging.debug("Saving as %s.jpg" % name)
+				
+				track.preview_img.save("%s.jpg" % name, File(img_temp), save=True)
+			except urllib2.URLError, exc:
+				logging.error("Exception occured when creating preview image: %s" % exc)
+		else:
+			logging.debug("Track has no GPS position data, not creating preview image")
+			# TODO: Maybe load fallback image as preview_image

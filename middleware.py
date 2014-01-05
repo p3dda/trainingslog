3f19bc0b2949aa886270e48ede710a07b795afda
Peter Adam <pedda@p3dda.net>
2014-01-05 15:19:30 +0100

    Fixed user agent detection if UA request header is missing

--- middleware.py
+++ middleware.py
@@ -1,16 +1,19 @@
 from django.conf import settings
 
 class MobileTemplatesMiddleware(object):
-    """Determines which set of templates to use for a mobile site"""
+	"""Determines which set of templates to use for a mobile site"""
 
-    ORIG_TEMPLATE_DIRS = settings.TEMPLATE_DIRS
+	ORIG_TEMPLATE_DIRS = settings.TEMPLATE_DIRS
 
-    def process_request(self, request):
-        ua_string = request.META['HTTP_USER_AGENT'].lower()
-        if 'ipad' in ua_string or 'iphone' in ua_string or 'android' in ua_string:	# FIXME Use list file
-            settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS + self.ORIG_TEMPLATE_DIRS
-            request.mobile=True
-        else:
-            settings.TEMPLATE_DIRS = self.ORIG_TEMPLATE_DIRS
-            request.mobile=False
+	def process_request(self, request):
+		if request.META.has_key('HTTP_USER_AGENT'):
+			ua_string = request.META['HTTP_USER_AGENT'].lower()
+		else:
+			ua_string = ''
+		if 'ipad' in ua_string or 'iphone' in ua_string or 'android' in ua_string:	# FIXME Use list file
+			settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS + self.ORIG_TEMPLATE_DIRS
+			request.mobile=True
+		else:
+			settings.TEMPLATE_DIRS = self.ORIG_TEMPLATE_DIRS
+			request.mobile=False
 

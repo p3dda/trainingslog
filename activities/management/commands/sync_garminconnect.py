e1283220418157a3facab859d28b2da7db66c5b8
Peter Adam <pedda@p3dda.net>
2014-11-16 11:10:41 +0100

    Code reformat, updated requirements.txt

--- activities/management/commands/sync_garminconnect.py
+++ activities/management/commands/sync_garminconnect.py
@@ -10,6 +10,7 @@ import requests
 import tempfile
 import time
 import urllib2
+import fcntl
 
 from django.core.management.base import BaseCommand, CommandError
 from activities.models import User
@@ -22,6 +23,9 @@ class Command(BaseCommand):
 	def __init__(self, *args, **kwargs):
 		super(Command, self).__init__(*args, **kwargs)
 		self.session = None
+		self.username = None
+		self.password = None
+
 		rate_lock_path = tempfile.gettempdir() + "/gc_rate.lock"
 		# Ensure the rate lock file exists (...the easy way)
 		open(rate_lock_path, "a").close()
@@ -29,7 +33,7 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		cj = cookielib.CookieJar()
-		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+		urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
 		if len(args) != 3:
 			CommandError("Invalid number of parameters")
@@ -38,36 +42,27 @@ class Command(BaseCommand):
 
 		print "Args: %s" % repr(args)
 		for user in users:
-			logging.debug("GarminConnect sync for user %s" % user.username)
+			logging.debug("GarminConnect sync for user %s", user.username)
 			self.session = None
 
 			self.username = args[1]  # FIXME: Take from user profile later
 			self.password = args[2]
 			self.session = self._get_session(email=self.username, password=self.password)
 
-			list = self.DownloadActivityList()
-			print "List is %s" % list
+			act_list = self.download_activity_list()
+			print "List is %s" % act_list
 
-	def _get_session(self, record=None, email=None, password=None, skip_cache=False):
-#		from tapiriik.auth.credential_storage import CredentialStore
-		# cached = self._sessionCache.Get(record.ExternalID if record else email)
-		# if cached and not skip_cache:
-		# 		return cached
-		# if record:
-		# 	#  longing for C style overloads...
-		# 	password = CredentialStore.Decrypt(record.ExtendedAuthorization["Password"])
-		# 	email = CredentialStore.Decrypt(record.ExtendedAuthorization["Email"])
-
+	def _get_session(self, email=None, password=None):
 		session = requests.Session()
 		self._rate_limit()
-		gcPreResp = session.get("http://connect.garmin.com/", allow_redirects=False)
+		gc_pre_resp = session.get("http://connect.garmin.com/", allow_redirects=False)
 		# New site gets this redirect, old one does not
-		if gcPreResp.status_code == 200:
+		if gc_pre_resp.status_code == 200:
 			self._rate_limit()
-			gcPreResp = session.get("https://connect.garmin.com/signin", allow_redirects=False)
-			req_count = int(re.search("j_id(\d+)", gcPreResp.text).groups(1)[0])
+			gc_pre_resp = session.get("https://connect.garmin.com/signin", allow_redirects=False)
+			req_count = int(re.search(r"j_id(\d+)", gc_pre_resp.text).groups(1)[0])
 			params = {"login": "login", "login:loginUsernameField": email, "login:password": password, "login:signInButton": "Sign In"}
-			auth_retries = 3 # Did I mention Garmin Connect is silly?
+			auth_retries = 3  # Did I mention Garmin Connect is silly?
 			for retries in range(auth_retries):
 				params["javax.faces.ViewState"] = "j_id%d" % req_count
 				req_count += 1
@@ -85,7 +80,7 @@ class Command(BaseCommand):
 					else:
 						raise CommandError("Mystery login error %s" % resp.text)
 				break
-		elif gcPreResp.status_code == 302:
+		elif gc_pre_resp.status_code == 302:
 			# JSIG CAS, cool I guess.
 			# Not quite OAuth though, so I'll continue to collect raw credentials.
 			# Commented stuff left in case this ever breaks because of missing parameters...
@@ -118,16 +113,16 @@ class Command(BaseCommand):
 				# "locale": "en"
 			}
 			# I may never understand what motivates people to mangle a perfectly good protocol like HTTP in the ways they do...
-			preResp = session.get("https://sso.garmin.com/sso/login", params=params)
-			if preResp.status_code != 200:
-				raise CommandError("SSO prestart error %s %s" % (preResp.status_code, preResp.text))
-			data["lt"] = re.search("name=\"lt\"\s+value=\"([^\"]+)\"", preResp.text).groups(1)[0]
+			pre_resp = session.get("https://sso.garmin.com/sso/login", params=params)
+			if pre_resp.status_code != 200:
+				raise CommandError("SSO prestart error %s %s" % (pre_resp.status_code, pre_resp.text))
+			data["lt"] = re.search(r"name=\"lt\"\s+value=\"([^\"]+)\"", pre_resp.text).groups(1)[0]
 
-			ssoResp = session.post("https://sso.garmin.com/sso/login", params=params, data=data, allow_redirects=False)
-			if ssoResp.status_code != 200:
-				raise CommandError("SSO error %s %s" % (ssoResp.status_code, ssoResp.text))
+			sso_resp = session.post("https://sso.garmin.com/sso/login", params=params, data=data, allow_redirects=False)
+			if sso_resp.status_code != 200:
+				raise CommandError("SSO error %s %s" % (sso_resp.status_code, sso_resp.text))
 
-			ticket_match = re.search("ticket=([^']+)'", ssoResp.text)
+			ticket_match = re.search("ticket=([^']+)'", sso_resp.text)
 			if not ticket_match:
 				raise CommandError("Invalid login")
 			ticket = ticket_match.groups(1)[0]
@@ -135,17 +130,17 @@ class Command(BaseCommand):
 			# ...AND WE'RE NOT DONE YET!
 
 			self._rate_limit()
-			gcRedeemResp1 = session.get("http://connect.garmin.com/post-auth/login", params={"ticket": ticket}, allow_redirects=False)
-			if gcRedeemResp1.status_code != 302:
-				raise CommandError("GC redeem 1 error %s %s" % (gcRedeemResp1.status_code, gcRedeemResp1.text))
+			gc_redeem_resp1 = session.get("http://connect.garmin.com/post-auth/login", params={"ticket": ticket}, allow_redirects=False)
+			if gc_redeem_resp1.status_code != 302:
+				raise CommandError("GC redeem 1 error %s %s" % (gc_redeem_resp1.status_code, gc_redeem_resp1.text))
 
 			self._rate_limit()
-			gcRedeemResp2 = session.get(gcRedeemResp1.headers["location"], allow_redirects=False)
-			if gcRedeemResp2.status_code != 302:
-				raise CommandError("GC redeem 2 error %s %s" % (gcRedeemResp2.status_code, gcRedeemResp2.text))
+			gc_redeem_resp2 = session.get(gc_redeem_resp1.headers["location"], allow_redirects=False)
+			if gc_redeem_resp2.status_code != 302:
+				raise CommandError("GC redeem 2 error %s %s" % (gc_redeem_resp2.status_code, gc_redeem_resp2.text))
 
 		else:
-			raise CommandError("Unknown GC prestart response %s %s" % (gcPreResp.status_code, gcPreResp.text))
+			raise CommandError("Unknown GC prestart response %s %s" % (gc_pre_resp.status_code, gc_pre_resp.text))
 
 		# self._sessionCache.Set(record.ExternalID if record else email, session)
 
@@ -153,30 +148,28 @@ class Command(BaseCommand):
 
 		return session
 
-	def DownloadActivityList(self, exhaustive=False):
-		#http://connect.garmin.com/proxy/activity-search-service-1.0/json/activities?&start=0&limit=50
-		#session = self._get_session(record=serviceRecord)
+	def download_activity_list(self):
+		#  http://connect.garmin.com/proxy/activity-search-service-1.0/json/activities?&start=0&limit=50
+		#  session = self._get_session(record=serviceRecord)
 		page = 1
-		pageSz = 100
-		activities = []
-		exclusions = []
+		page_sz = 100
 		while True:
-			logging.debug("Req with " + str({"start": (page - 1) * pageSz, "limit": pageSz}))
+			logging.debug("Req with " + str({"start": (page - 1) * page_sz, "limit": page_sz}))
 			self._rate_limit()
 
 			retried_auth = False
+			res = None
 			while True:
-				res = self.session.get("http://connect.garmin.com/proxy/activity-search-service-1.0/json/activities", params={"start": (page - 1) * pageSz, "limit": pageSz})
+				res = self.session.get("http://connect.garmin.com/proxy/activity-search-service-1.0/json/activities", params={"start": (page - 1) * page_sz, "limit": page_sz})
 				# It's 10 PM and I have no clue why it's throwing these errors, maybe we just need to log in again?
 				if res.status_code == 403 and not retried_auth:
 					retried_auth = True
-					session = self._get_session(email=self.username, password=self.password)
+					self.session = self._get_session(email=self.username, password=self.password)
 				else:
 					break
 			try:
 				res = res.json["results"]
 			except ValueError:
-				res_txt = res.text # So it can capture in the log message
 				raise CommandError("Parse failure in GC list resp: %s" % res.status_code)
 			if "activities" not in res:
 				break  # No activities on this page - empty account.
@@ -194,10 +187,9 @@ class Command(BaseCommand):
 			break
 
 	def _rate_limit(self):
-		import fcntl, struct, time
 		min_period = 1  # I appear to been banned from Garmin Connect while determining this.
 		print("Waiting for lock")
-		fcntl.flock(self._rate_lock,fcntl.LOCK_EX)
+		fcntl.flock(self._rate_lock, fcntl.LOCK_EX)
 		try:
 			print("Have lock")
 			self._rate_lock.seek(0)
@@ -216,4 +208,4 @@ class Command(BaseCommand):
 
 			print("Rate limited for %f" % wait_time)
 		finally:
-			fcntl.flock(self._rate_lock,fcntl.LOCK_UN)
+			fcntl.flock(self._rate_lock, fcntl.LOCK_UN)

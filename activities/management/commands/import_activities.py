985d8071fdf78940885d99689a4c47c0422faee1
Peter Adam <pedda@p3dda.net>
2015-03-03 22:52:12 +0100

    Add dynamic userprofile parameters, add imap import plugin

---
+++ activities/management/commands/import_activities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Import activities from external sources
+"""
+import logging
+
+
+from django.core.management.base import BaseCommand
+from activities.models import User
+
+import activities.extras.syncplugins.imap
+
+
+class Command(BaseCommand):
+	help = 'Sync users activities configured external sources'
+
+	def handle(self, *args, **options):
+		for user in User.objects.all():
+			logging.debug("Call sync handlers for user %s" % user.username)
+
+			plugin = activities.extras.syncplugins.imap.IMAPSync(user)
+			plugin.run()

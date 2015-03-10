2b13d7acf614a4005d7febcd51c91e12658ad326
Peter Adam <pedda@p3dda.net>
2015-03-10 21:37:00 +0100

    Switched from UserProfile to generic key-value pair storage for user parameters

--- activities/extras/syncplugins/imap.py
+++ activities/extras/syncplugins/imap.py
@@ -24,73 +24,95 @@ class IMAPSyncException(Exception):
 
 class IMAPSync(object):
 	def __init__(self, user):
+		"""
+		IMAPSync __init__ method
+		@param user: django user instance
+		"""
 		self.user = user
 		self.imapclient = None
 		logging.debug("Initialize IMAP sync for user %s" % self.user.username)
 
 	def run(self):
-		profile = self.user.profile
-		if profile.params:
-			if profile.params['trainingslog.sync.imap.enable'] == 'True':
-				logging.debug("IMAP sync is enabled for user %s" % self.user.username)
+		"""
+		IMAP import plugin main
+		"""
+		# profile = self.user.profile
+		if 'sync.imap.enable' in self.user.params and self.user.params['sync.imap.enable'] == 'True':
+			logging.debug("IMAP sync is enabled for user %s" % self.user.username)
 
-				self.imapclient = imaplib.IMAP4(profile.params['trainingslog.sync.imap.host'])
-				result = self.imapclient.login(profile.params['trainingslog.sync.imap.user'], profile.params['trainingslog.sync.imap.password'])
-				if result[0] != 'OK':
-					raise IMAPSyncException('IMAP login failed: %s' % result)
+			self.imapclient = imaplib.IMAP4(self.user.params['sync.imap.host'])
+			result = self.imapclient.login(self.user.params['sync.imap.user'], self.user.params['sync.imap.password'])
+			if result[0] != 'OK':
+				raise IMAPSyncException('IMAP login failed: %s' % result)
 
-				result = self.imapclient.select(profile.params['trainingslog.sync.imap.mailbox'])
-				if result[0] != 'OK':
-					raise IMAPSyncException('IMAP failed to select mailbox %s: %s' % (profile.params['trainingslog.sync.imap.mailbox'], result))
+			result = self.imapclient.select(self.user.params['sync.imap.mailbox'])
+			if result[0] != 'OK':
+				raise IMAPSyncException('IMAP failed to select mailbox %s: %s' % (self.user.params['sync.imap.mailbox'], result))
 
-				typ, data = self.imapclient.search(None, 'ALL')
-				for num in data[0].split():
-					_, mail_raw = self.imapclient.fetch(num, '(RFC822)')
-					mail = email.message_from_string(mail_raw[0][1])
-					result = self.process_email(mail)
-					if result:
-						logging.debug("EMail imported successfully, mark as deleted")
-						self.imapclient.store(num, '+FLAGS', '\\Deleted')
+			typ, data = self.imapclient.search(None, 'ALL')
 
-				self.imapclient.expunge()
-			else:
-				logging.debug("IMAP sync is not enabled for user %s" % self.user.username)
-				return
+			num_processed = 0
+			num_activities = 0
+			for num in data[0].split():
+				_, mail_raw = self.imapclient.fetch(num, '(RFC822)')
+				mail = email.message_from_string(mail_raw[0][1])
+				(result, num_act) = self.process_email(mail)
+				if result:
+					logging.debug("EMail imported successfully, mark as deleted")
+					num_processed += 1
+					num_activities += num_act
+					self.imapclient.store(num, '+FLAGS', '\\Deleted')
+
+			# TODO: Send confirmation mail with number of processed emails and imported activities
+			self.imapclient.expunge()
 		else:
 			logging.debug("IMAP sync is not enabled for user %s" % self.user.username)
+			return
 		return
 
 	def process_email(self, mail):
+		"""
+		Process raw email and import activities from attached files
+		@param mail: email as string
+		@return: Tuple: (success, num) Boolean success flag and number of imported activities
+		"""
 		email_success = False
+		num_act = 0
 		logging.debug("Parsing email: From: %s, Subject: %s, Date: %s" % (mail["From"], mail["Subject"], mail["Date"]))
 		if not mail.is_multipart():
 			logging.debug("Mail has no attachment, skip")
 			return
+
+		tmpdirname = tempfile.mkdtemp()
 		for part in mail.walk():
 			ctype = part.get_content_type()
-			filename = filename = part.get_filename()
+			filename = part.get_filename()
 			if ctype == 'application/octet-stream' and filename:
 				logging.debug("Found .fit attachment with filename %s" % filename)
-				# Call fit file import
+				# Call file import
 				if not mail["Subject"]:
 					name = 'IMAP Import'
 				else:
 					name = mail["Subject"]
-				result = self.import_fit_file(part, name)
+
+				tmpfile = os.path.join(tmpdirname, part.get_filename())
+				with open(tmpfile, 'wb') as f:
+					f.write(part.get_payload(decode=True))
+
+				result = self.import_file(tmpfile, name)
 				if result:
 					email_success = True
-		return email_success
+					num_act += 1
+		shutil.rmtree(tmpdirname)
 
-	def import_fit_file(self, part, name):
-		tmpdirname = tempfile.mkdtemp()
+		return (email_success, num_act)
+
+	def import_file(self, actfile, name):
 		is_saved = False
-		fitfile = os.path.join(tmpdirname, part.get_filename())
-		open(fitfile, 'wb').write(part.get_payload(decode=True))
 		newtrack = Track()
-		_, fileextension = os.path.splitext(fitfile)
+		_, fileextension = os.path.splitext(actfile)
 		newtrack.filetype = fileextension.lower()[1:]
-		newtrack.trackfile.save(fitfile, File(open(fitfile), 'r'))
-
+		newtrack.trackfile.save(actfile, File(open(actfile), 'r'))
 		is_saved = True
 		try:
 			activityfile = ActivityFile.ActivityFile(newtrack, user=self.user, activityname=name)
@@ -104,5 +126,4 @@ class IMAPSync(object):
 			for line in traceback.format_exc().splitlines():
 				logging.error(line.strip())
 
-		shutil.rmtree(tmpdirname)
 		return True

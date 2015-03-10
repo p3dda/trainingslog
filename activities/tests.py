a52d81a81f5e70798df0e999e54aed138661f04b
Peter Adam <pedda@p3dda.net>
2015-03-10 21:52:30 +0100

    Added basic TestCase for user parameters

--- activities/tests.py
+++ activities/tests.py
@@ -17,8 +17,29 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
 
+from django.contrib.auth.models import User
 from django.conf import settings as django_settings
 from django.core.urlresolvers import reverse
+
+
+class UserTest(TestCase):
+	fixtures = ['activities_testdata.json', 'activities_tests_authdata.json']
+
+	def setUp(self):
+		self.client = django.test.client.Client()
+
+	def test_user_parameters(self):
+		user = User.objects.get(username='test1')
+		params = user.params
+		self.assertEqual({}, params.asPyDict())
+
+		params['foo'] = 'bar'
+		self.assertEqual({'foo': 'bar'}, params.asPyDict())
+		self.assertEqual(user.params['foo'], 'bar')
+		self.assertListEqual(user.params.keys(), ['foo'])
+
+		del params['foo']
+		self.assertListEqual(user.params.keys(), [])
 
 
 class ActivityTest(TestCase):
